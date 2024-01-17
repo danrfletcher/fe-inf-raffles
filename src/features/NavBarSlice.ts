bbd9a3b953848a0156208e12c20366eb0434ebe8
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-17 16:13:55 +0000

    Add async thunk to nav bar slice for fetching navPages

--- src/features/NavBarSlice.ts
+++ src/features/NavBarSlice.ts
@@ -1,13 +1,37 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { NavPagesObject, getNavPages } from "../services/get-nav-pages";
+
+class NavPagesError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NavPagesError';
+  }
+}
+
+export const fetchNavPages = createAsyncThunk(
+  "nav/fetchNavPages", 
+  async (): Promise<NavPagesObject> => {
+  try {
+    const data = await getNavPages();
+    return data;
+  } catch (e) {
+    console.log(e);
+    throw new NavPagesError("Unable to get navigation pages");
+  }
+})
 
 interface NavState {
   mobileBarIsOpen: boolean,
   notYetLoaded: boolean,
+  navPages: NavPagesObject | null
+  navPageError: NavPagesError | null
 }
 
 const initialState: NavState = {
     mobileBarIsOpen: false,
     notYetLoaded: true,
+    navPages: null,
+    navPageError: null
 }
 
 const navSlice = createSlice({
@@ -19,7 +43,16 @@ const navSlice = createSlice({
     },
     setNotYetLoaded: (state) => {
       state.notYetLoaded = false;
-    }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchNavPages.fulfilled, (state, action: PayloadAction<NavPagesObject>) => {
+      state.navPages = action.payload;
+      state.navPageError = null; 
+    });
+    builder.addCase(fetchNavPages.rejected, (state, action) => {
+      state.navPageError = new NavPagesError(action.error.message || "Unable to fetch nav pages");
+    });
   }
 })
 
